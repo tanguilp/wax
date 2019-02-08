08eb43b7350ca480271c148a78dccf76001b6942
Tangui <29804907+tanguilp@users.noreply.github.com>
2019-02-09 00:35:08 +0300

    Added credential id type definition

---
+++ lib/wax/credential_id.ex
@@ -0,0 +1,14 @@
+defmodule Wax.CredentialId do
+  @typedoc """
+  The binary representation of a credential id
+
+  It is usually transmitted to a FIDO2 RP encoded as a base 64 string. One can decode it as
+  follows:
+
+  ```elixir
+  Base.url_decode64!(credential_id, padding: false)
+  ```
+  """
+
+  @type t :: binary()
+end
