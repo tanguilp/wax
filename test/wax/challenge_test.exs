71b259b7719afba0a8b386f7ff69dcb385f1aa9c
armanm <arman.mirkzemi@gmail.com>
2025-03-04 09:28:35 +1100

    fix typo in name

--- test/wax/challenge_test.exs
+++ test/wax/challenge_test.exs
@@ -1,13 +1,15 @@
-defmodule Wax.ChanllengeTest do
+defmodule Wax.ChallengeTest do
   use ExUnit.Case, async: true
 
   alias Wax.Challenge
 
-  test "generates bytes" do
-    assert Challenge.new([]).bytes
-  end
+  describe "new/1" do
+    test "generates bytes" do
+      assert Challenge.new([]).bytes
+    end
 
-  test "does not override bytes when provided as an option" do
-    assert %{bytes: "abcd"} = Challenge.new(bytes: "abcd")
+    test "does not override bytes when provided as an option" do
+      assert %{bytes: "abcd"} = Challenge.new(bytes: "abcd")
+    end
   end
 end
